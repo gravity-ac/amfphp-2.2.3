3c6940c65f9cd0b9295ca7c2a6939a1989298fe1
gravity-ac <39431568+gravity-ac@users.noreply.github.com>
2023-10-17 20:24:02 -0500

    Update

--- Amfphp/index.php
+++ Amfphp/index.php
@@ -1,33 +1,17 @@
 <?php
 /**
- *  This file is part of amfPHP
+ *  This file is part of amfPHP installed with Composer
  *
- * LICENSE
- *
- * This source file is subject to the license that is bundled
- * with this package in the file license.txt.
- */
+ */ 
+ 
+require ("../../app/autoload.php");
+$serviceFolderLocation = "/var/www/help.museapp.ai/services/";
 
-/**
-*  includes
- * @package Amfphp
-*  */
-require_once dirname(__FILE__) . '/ClassLoader.php';
+$config = new Amfphp_Core_Config();
+$config->serviceFolders = array($serviceFolderLocation);
 
-/* 
- * main entry point (gateway) for service calls. instanciates the gateway class and uses it to handle the call.
- * 
- * @package Amfphp
- * @author Ariel Sommeria-klein
- */
-$gateway = Amfphp_Core_HttpRequestGatewayFactory::createGateway();
-
-//use this to change the current folder to the services folder. Be careful of the case.
-//This was done in 1.9 and can be used to support relative includes, and should be used when upgrading from 1.9 to 2.0 if you use relative includes
-//chdir(dirname(__FILE__) . '/Services');
-
+$gateway = Amfphp_Core_HttpRequestGatewayFactory::createGateway($config);
 $gateway->service();
 $gateway->output();
-
 
 ?>
